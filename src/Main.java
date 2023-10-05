767fcc34811b3850b12aef4767495b39884aeb08
bramoscazares <bramoscazares@ggc.edu>
2023-10-05 18:24:07 -0400

    Created a Method to print a message in Main.class -Brian

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 
+    public static void printBrianMessage(){
+        System.out.println("Crocodiles are cool. :)");
+    }
 
+    public static void main(String[] args) {
+        printBrianMessage();
+    }
 
 }
