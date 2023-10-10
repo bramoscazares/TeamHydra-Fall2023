15649d9a42b8bbcf40e6fa8a16fccc4cdb64089b
MHopkins1gg <mhopkins1@ggc.edu>
2023-10-09 20:19:52 -0400

    Added a cat class that has a name string and speak method

---
+++ src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String name;
+
+    public void speak() {
+        System.out.println(this.name + "meows at you.");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
